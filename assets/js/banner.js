fa405e66807cc8028cc94c86445477dcdf5f4812
Gabi Lemos <gabicarvalholemos@gmail.com>
2022-09-16 20:07:09 -0300

    created placeholder

--- assets/js/banner.js
+++ assets/js/banner.js
@@ -1,4 +1,13 @@
-$('.galery').slick({
-  autoplay: true,
-  autoplaySpeed: 3000
+let placeholderBanner = document.querySelector('#carousel-placeholder')
+let carouselGalery = document.querySelector('#carousel-galery')
+
+document.addEventListener('DOMContentLoaded', function () {
+  setTimeout(() => {
+    placeholderBanner.style.display = 'none'
+    carouselGalery.style.visibility = 'visible'
+    $('.galery').slick({
+      autoplay: true,
+      autoplaySpeed: 3000
+    })
+  }, 2000)
 })
